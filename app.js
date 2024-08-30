14e1f5d071ce718bb3a1d62c852a62a27be8511a
pieroblunda <pieroblunda@gmail.com>
2024-08-30 15:49:32 +0200

    Add Css

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import VespaJs from 'vespa-framework';
 
 VespaJs.core.setGlobalsVariables();
+global.PUBLIC_PATH='/Users/pieroblunda/Sites/blog-saas/public/_sanitydrivendevelopment';
 VespaJs.stylus.compileStylus();
